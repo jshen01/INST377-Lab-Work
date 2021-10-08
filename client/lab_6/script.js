6e142fb29dc78d1fab2b61f817650fd39f46173e
jshen01 <jshen01@terpmail.umd.edu>
2021-10-08 19:28:06 -0400

    Lab 6

---
+++ client/lab_6/script.js
@@ -0,0 +1,38 @@
+async function windowActions(evt) {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; /* '/api/wholeMeal' */
+    const request = await fetch(endpoint);
+    const locations = await request.json();
+
+    function findMatches(wordToMatch, locations) {
+        return locations.filter((place) => {
+            const regex = new RegExp(wordToMatch, 'gi');
+            // Search by name, city, and zipcode
+            return place.name.match(regex) || place.city.match(regex) || place.zip.match(regex);
+        });
+    }
+
+    // Demo video returned name, category, address, city, zipcode
+    function displayMatches(event) {
+        const matchArray = findMatches(event.target.value, locations);
+        const html = matchArray.map(place => {
+            return `
+          <ul>
+            <li><div class='name'>${place.name}</div></li>
+            <div class='category'>${place.category}</div>
+            <div class='address'>${place.address_line_1}</div>
+            <div class='city'>${place.city}</div>
+            <div class='zip'>${place.zip}</div>
+          </ul>
+          <br> 
+          `;
+        }).join('');
+        suggestions.innerHTML = html;
+    }
+
+    const searchInput = document.querySelector('.search');
+    const suggestions = document.querySelector('.suggestions');
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+}
+
+window.onload = windowActions;
